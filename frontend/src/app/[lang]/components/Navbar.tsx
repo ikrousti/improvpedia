5584f48fc6e7657cf6bff3ee789bc8dcde2afb5c
Iphigeneia Krousti <iphigeneiakr@gmail.com>
2023-06-27 12:42:35 +0300

    Changed the blog route

--- frontend/src/app/[lang]/components/Navbar.tsx
+++ frontend/src/app/[lang]/components/Navbar.tsx
@@ -52,13 +52,13 @@ export default function Navbar({
     </div>
     <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div className="text-sm lg:flex-grow">
-        <a href="/blog/shortform" className="block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-white mr-4">
+        <a href="/Games/shortform" className="block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-white mr-4">
           Shortform
         </a>
-        <a href="/blog/longform" className="block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-white mr-4">
+        <a href="/Games/longform" className="block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-white mr-4">
           Longform
         </a>
-        <a href="/blog/mediumform" className="block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-white">
+        <a href="/Games/mediumform" className="block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-white mr-4">
           Mediumform
         </a>
       </div>
